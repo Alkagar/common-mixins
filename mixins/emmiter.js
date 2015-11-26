13dafe29e999ddf3cd6db5c125be5c931a15eb85
Jakub Mrowiec <alkagar@gmail.com>
2015-11-26 14:14:41 +0100

    update emmiter.

--- mixins/emmiter.js
+++ mixins/emmiter.js
@@ -4,7 +4,6 @@ var EventEmitter = require('events').EventEmitter;
 module.exports = function(obj, setup) {
     'use strict';
     var emmiter = new EventEmitter();
-    console.log(emmiter);
     obj.emmiter = emmiter;
     return obj;
 };
