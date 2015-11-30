8873ef559254fc25441fa2559b11ce2d0f46aee7
Jakub Mrowiec <alkagar@gmail.com>
2015-11-30 15:21:14 +0100

    rename logger module.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var mixinLogger = require('./mixins/logger.js');
+var mixinLogger = require('./mixins/simple-logger.js');
 var mixinRedis = require('./mixins/redis.js');
 var mixinEmmiter = require('./mixins/emmiter.js');
 var simpleHttp = require('./http/simple-http.js');
